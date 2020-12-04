0e61177119894b461c01b56b74c57bd3ff41fdc4
Shashank Biplav <Biplavshashank7@gmail.com>
2020-12-04 23:34:27 +0530

    [Feature:Full Stack] Added admin views and fixed redirect to /admin route after login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin',[AdminController::class, 'index'])->name('admin.index');
