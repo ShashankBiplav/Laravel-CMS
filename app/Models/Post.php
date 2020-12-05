61ade8e5afc554cd5f550b863f1764108f9cae55
Shashank Biplav <Biplavshashank7@gmail.com>
2020-12-05 15:53:01 +0530

    [DB:Seeding] Created database seeders

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,13 @@ class Post extends Model
 {
   use HasFactory;
 
+  protected $fillable = [
+    'user_id',
+    'title',
+    'post_image',
+    'body'
+  ];
+
   public function user()
   {
     return $this->belongsTo(User::class);
