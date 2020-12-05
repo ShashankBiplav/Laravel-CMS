38a6c8a029697bc166c2189e1f177573da6b68d4
Shashank Biplav <Biplavshashank7@gmail.com>
2020-12-05 14:16:43 +0530

    [Feature] Added post migration and route and view to view a single post

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PostController;
 
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/admin',[AdminController::class, 'index'])->name('admin.index');
+
+Route::get('/post', [PostController::class, 'show'])->name('blog.post');
