7eb0f202b1040dd9457018edf890f9eecc0517e0
Shashank Biplav <Biplavshashank7@gmail.com>
2020-12-12 13:14:08 +0530

    [Client] Seperated the top navbar user info component

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -197,7 +197,7 @@
                   </div>
                 </div>
                 <div>
-                  <div class="small text-gray-500">December 12, 2019</div>
+                  <div class="small text-gray-500">December 12, 2020</div>
                   <span class="font-weight-bold">A new monthly report is ready to download!</span>
                 </div>
               </a>
@@ -219,7 +219,7 @@
                   </div>
                 </div>
                 <div>
-                  <div class="small text-gray-500">December 2, 2019</div>
+                  <div class="small text-gray-500">December 2, 2020</div>
                   Spending Alert: We've noticed unusually high spending for your account.
                 </div>
               </a>
@@ -286,32 +286,9 @@
           <div class="topbar-divider d-none d-sm-block"></div>
 
           <!-- Nav Item - User Information -->
-          <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-              <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
-            </a>
-            <!-- Dropdown - User Information -->
-            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-              <a class="dropdown-item" href="#">
-                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                Profile
-              </a>
-              <a class="dropdown-item" href="#">
-                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                Settings
-              </a>
-              <a class="dropdown-item" href="#">
-                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                Activity Log
-              </a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                Logout
-              </a>
-            </div>
-          </li>
+          <x-admin-top-navbar-user-information>
+
+          </x-admin-top-navbar-user-information>
 
         </ul>
 
