f5b5a00a781f3c6f7529fe8bc1e58ca31a2de210
Shashank Biplav <Biplavshashank7@gmail.com>
2020-12-12 16:02:36 +0530

    [Client] Added template for user profile

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -1,8 +1,33 @@
 <x-admin-master>
   @section('content')
-    <h1>Profile</h1>
-        <h2>{{$user->name}}</h2>
-        <h2>{{$user->id}}</h2>
-        <h2>{{$user->email}}</h2>
+    <h1>Your Profile</h1>
+        <img height="80px" width="80px" class="img-profile rounded-circle my-4" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+        <div class="row">
+            <div class="col-sm-6">
+                <form action="" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <input type="file">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" name="name" id="name" value="{{$user->name}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" name="email" id="email" value="{{$user->email}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name="password" id="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="confirm-password">Confirm Password</label>
+                        <input type="password" class="form-control" name="confirm-password" id="confirm-password">
+                    </div>
+                    <button class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
     @endsection
 </x-admin-master>
